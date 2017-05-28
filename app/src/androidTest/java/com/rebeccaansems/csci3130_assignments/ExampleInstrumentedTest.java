4febefdfcccdb2ea41a3f9a9993aeb5189e2192e
Rebecca Ansems <ansems_rebecca@hotmail.com>
2017-05-28 15:44:01 -0300

    Added UI for password validation

--- app/src/androidTest/java/com/rebeccaansems/csci3130_assignments/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/rebeccaansems/csci3130_assignments/ExampleInstrumentedTest.java
@@ -1,26 +1,59 @@
 package com.rebeccaansems.csci3130_assignments;
 
-import android.content.Context;
-import android.support.test.InstrumentationRegistry;
-import android.support.test.runner.AndroidJUnit4;
-
+import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import static org.junit.Assert.*;
+import android.support.test.espresso.ViewAction;
+import android.support.test.filters.LargeTest;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.view.KeyEvent;
+import android.widget.EditText;
 
-/**
- * Instrumentation test, which will execute on an Android device.
- *
- * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
- */
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.pressKey;
+import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isAssignableFrom;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+
+
+import static android.support.test.espresso.action.ViewActions.click;
+
+
 @RunWith(AndroidJUnit4.class)
+@LargeTest
 public class ExampleInstrumentedTest {
-    @Test
-    public void useAppContext() throws Exception {
-        // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.rebeccaansems.csci3130_assignments", appContext.getPackageName());
+    private String textToFind;
+
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityRule = new ActivityTestRule<>(
+            MainActivity.class);
+
+    @Test
+    public void passwordIsWeak()
+    {
+        onView(withId(R.id.testInputPassword)).perform(click());
+        onView(isAssignableFrom(EditText.class)).perform(typeText("badPassword"));
+        onView(withId(R.id.buttonValidatePassword)).perform(click());
+
+        onView(withId(R.id.textViewPasswordStrength)).check(matches(withText("Password is weak")));
     }
+
+    @Test
+    public void passwordIsStrong()
+    {
+        onView(withId(R.id.testInputPassword)).perform(click());
+        onView(isAssignableFrom(EditText.class)).perform(typeText("thisIsAG00dP@ssword"));
+        onView(withId(R.id.buttonValidatePassword)).perform(click());
+
+        onView(withId(R.id.textViewPasswordStrength)).check(matches(withText("Password is strong")));
+    }
+
+
 }
--- app/src/main/java/com/rebeccaansems/csci3130_assignments/MainActivity.java
+++ app/src/main/java/com/rebeccaansems/csci3130_assignments/MainActivity.java
@@ -1,15 +1,38 @@
 package com.rebeccaansems.csci3130_assignments;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button validateButton = (Button)findViewById(R.id.buttonValidatePassword);
+        validateButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v){
+                validateButtonClicked();
+            }
+        });
     }
+
+
+    public void validateButtonClicked(){
+        if(passwordValidator(((EditText)findViewById(R.id.testInputPassword)).getText().toString())){
+            ((TextView)findViewById(R.id.textViewPasswordStrength)).setText("Password is strong");
+        }
+        else {
+            ((TextView)findViewById(R.id.textViewPasswordStrength)).setText("Password is weak");
+        }
+    }
+
 
     public static boolean passwordValidator(String password){
         if(password.length() > 8 &&
